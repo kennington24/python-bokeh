64cdc1afc271af3f98267900c84a573feb8c9418
Kenny <khpham24@gmail.com>
2018-06-20 16:16:16 -0600

    getting labels to print on 0,0

--- src/draw.py
+++ src/draw.py
@@ -1,21 +1,33 @@
 import math
 
 from bokeh.io import show, output_file
-from bokeh.plotting import figure
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.plotting import figure, show, output_file
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, ColumnDataSource, LabelSet, Label
 from bokeh.palettes import Spectral8
 
-N = 8
+from graph import *
+
+source = ColumnDataSource(data=dict(names=['1']))
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+print(graph_data.vertexes)
+
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+color_list = []
+for vertex in graph_data.vertexes:
+  color_list.append(vertex.color)
+
+plot = figure(title='Graph Layout Demonstration', x_range=(-1, 1), y_range=(-1, 1),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(Spectral8, 'color')
-graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+graph.node_renderer.data_source.add(color_list, 'color')
+graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
 graph.edge_renderer.data_source.data = dict(
     start=[0]*N,
@@ -23,14 +35,16 @@ graph.edge_renderer.data_source.data = dict(
 
 ### start of layout code
 #setting the postion of the vertexes
-circ = [i*2*math.pi/8 for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
+labels = LabelSet(text='names', level='glyph', x_offset=5, y_offset=5, source=source, render_mode='canvas')
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
+plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
