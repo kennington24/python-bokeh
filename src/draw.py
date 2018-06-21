79959d0c944f8e005beb36c71dbfc40f870d89dd
Kenny <khpham24@gmail.com>
2018-06-21 16:37:54 -0600

    labels added, randomize working

--- src/draw.py
+++ src/draw.py
@@ -2,16 +2,15 @@ import math
 
 from bokeh.io import show, output_file
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval, ColumnDataSource, LabelSet, Label
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Circle, ColumnDataSource, LabelSet, Label
 from bokeh.palettes import Spectral8
 
 from graph import *
 
-source = ColumnDataSource(data=dict(names=['1']))
-
 graph_data = Graph()
 graph_data.debug_create_test_data()
-print(graph_data.vertexes)
+graph_data.randomize(12, 9, 150, 20)
+graph_data.bfs(graph_data.vertexes[0])
 
 N = len(graph_data.vertexes)
 node_indices = list(range(N))
@@ -20,31 +19,42 @@ color_list = []
 for vertex in graph_data.vertexes:
   color_list.append(vertex.color)
 
-plot = figure(title='Graph Layout Demonstration', x_range=(-250, 400), y_range=(-250, 400),
+plot = figure(title='Graph Layout Demonstration', x_range=(-300, 420), y_range=(-300, 420),
               tools='', toolbar_location=None)
 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
 graph.node_renderer.data_source.add(color_list, 'color')
-graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
+graph.node_renderer.glyph = Circle(size=30, fill_color='color')
 
-graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+graph.edge_renderer.data_source.data = dict(start=[], end=[])
+
+for vertex in graph_data.vertexes:
+
+  for edge in vertex.edges:
+    start = graph_data.vertexes.index(vertex)
+    graph.edge_renderer.data_source.data['start'].append(start)
+
+    end = graph_data.vertexes.index(edge.destination)
+    graph.edge_renderer.data_source.data['end'].append(end)
 
 ### start of layout code
 #setting the postion of the vertexes
 x = [v.pos['x'] for v in graph_data.vertexes]
 y = [v.pos['y'] for v in graph_data.vertexes]
+values = [v.value for v in graph_data.vertexes]
 
-labels = LabelSet(text='names', level='glyph', x_offset=5, y_offset=5, source=source, render_mode='canvas')
+source = ColumnDataSource(data=dict(x=x, y=y, values=values))
 
+labels = LabelSet(x='x', y='y', text='values', level='overlay',
+                  text_align='center', text_baseline='middle', source=source, render_mode='canvas')
+
+plot.add_layout(labels)
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
 
 plot.renderers.append(graph)
-plot.add_layout(labels)
 
 output_file('graph.html')
 show(plot)
--- src/graph.py
+++ src/graph.py
@@ -1,11 +1,13 @@
+import random
+
 class Edge:
   def __init__(self, destination):
     self.destination = destination
 
 class Vertex:
-  def __init__(self, value, **pos):
+  def __init__(self, value, color, **pos):
     self.value = value
-    self.color = 'white'
+    self.color = color
     self.pos = pos
     self.edges =[]
 
@@ -14,9 +16,9 @@ class Graph:
     self.vertexes = []
 
   def debug_create_test_data(self):
-    debug_vertex_1 = Vertex('t1', x=40, y=40)
-    debug_vertex_2 = Vertex('t2', x=140, y=140)
-    debug_vertex_3 = Vertex('t3', x=300, y=400)
+    debug_vertex_1 = Vertex('t1', 'red', x=40, y=40)
+    debug_vertex_2 = Vertex('t2', 'blue', x=140, y=140)
+    debug_vertex_3 = Vertex('t3', 'pink', x=300, y=400)
 
     debug_edge_1 = Edge(debug_vertex_2)
     debug_vertex_1.edges.append(debug_edge_1)
@@ -25,3 +27,68 @@ class Graph:
     debug_vertex_3.edges.append(debug_edge_2)
 
     self.vertexes.extend([debug_vertex_1, debug_vertex_2, debug_vertex_3])
+
+  def bfs(self, start):
+        random_color = "#" + \
+            ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+
+        queue = []
+        found = []
+        found.append(start)
+        queue.append(start)
+
+        start.color = random_color
+
+        while (len(queue) > 0):
+            v = queue[0]
+            for edge in v.edges:
+                if edge.destination not in found:
+                    found.append(edge.destination)
+                    queue.append(edge.destination)
+                    edge.destination.color = random_color
+            queue.pop(0)
+        return found
+
+  def randomize(self, width, height, pxBox, probability):
+        def connectVerts(v0, v1):
+            v0.edges.append(Edge(v1))
+            v1.edges.append(Edge(v0))
+
+        count = 0
+        grid = []
+
+        for y in range(height):
+            row = []
+            for x in range(width):
+                v = Vertex('default', 'white', x=200, y=200)
+                v.value = f"v{count}"
+                count += 1
+                row.append(v)
+            grid.append(row)
+        # print(grid)
+
+        for y in range(height):
+            for x in range(width):
+                if (y < height - 1):
+                    if random.randint(0, 100) < probability:
+                        print(f"testing data       x: {x}  y: {y}")
+                        connectVerts(grid[y][x], grid[y+1][x])
+                if (x < width - 1):
+                    if random.randint(0, 100) < probability:
+                        connectVerts(grid[y][x], grid[y][x+1])
+
+        boxBuffer = 0.8
+        boxInner = pxBox * boxBuffer
+        boxInnerOffset = (pxBox - boxInner) / 2
+
+        for y in range(height):
+            for x in range(width):
+                # print(grid[y][x].pos)
+                grid[y][x].pos['x'] = (
+                    x * pxBox + boxInnerOffset + (random.randint(0, 100) / 100) * boxInner) % 1
+                grid[y][x].pos['y'] = (
+                    y * pxBox + boxInnerOffset + (random.randint(0, 100) / 100) * boxInner) % 1
+
+        for y in range(height):
+            for x in range(width):
+                self.vertexes.append(grid[y][x])
